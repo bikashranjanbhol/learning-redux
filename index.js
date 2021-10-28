5dec3d65c9c77b1c9e5f79ffca98f891f42f8315
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-28 11:26:31 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import { createStore } from 'redux';
+import './redux-core/basic.js';
+// import { createStore } from 'redux';
 
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 };
-    case 'counter/decremented':
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+// function counterReducer(state = { value: 0 }, action) {
+//   switch (action.type) {
+//     case 'counter/incremented':
+//       return { value: state.value + 1 };
+//     case 'counter/decremented':
+//       return { value: state.value - 1 };
+//     default:
+//       return state;
+//   }
+// }
 
-let store = createStore(counterReducer)
+// let store = createStore(counterReducer)
 
-store.subscribe(() => console.log(store.getState()))
+// store.subscribe(() => console.log(store.getState()))
 
-store.dispatch({ type: 'counter/incremented' })
+// store.dispatch({ type: 'counter/incremented' })
 
-store.dispatch({ type: 'counter/incremented' })
+// store.dispatch({ type: 'counter/incremented' })
+
+// console.log(document.getElementById('app'));
