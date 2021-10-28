35b0b9a9b0dc44d265003b08887cd20b8f099d98
bikashranjanbhol <bikashbholranjan@gmail.com>
2021-10-28 11:18:14 +0530

    added an example

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
+import { createStore } from 'redux';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 };
+    case 'counter/decremented':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+
+let store = createStore(counterReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({ type: 'counter/incremented' })
+
+store.dispatch({ type: 'counter/incremented' })
